b1684fd3c5cc0457a68049791c28e039b7696384
alfston <alfstonthomas@gmail.com>
2020-12-01 12:44:26 -0500

    Final

--- daos/questions-dao.js
+++ daos/questions-dao.js
@@ -1,7 +1,7 @@
 const questionsModel = require('../models/questions/questions-model.js')
 const findAllQuestions = () => questionsModel.find()
-const findQuestionsById = (quizId) => questionsModel.findById(quizId)
-const findQuestionsForQuiz = (quizId) => questionsModel.findById(quizId)
-module.exports = { findAllQuestions, findQuestionsById }
+const findQuestionsById = (qid) => questionsModel.findById(qid)
+const findQuestionsForQuiz = (qzid) => questionsModel.findById(qzid)
+module.exports = { findAllQuestions, findQuestionsById, findQuestionsForQuiz }
 
 
